aa2a2cf6483e68257f6c3cbc437e99cbe6637e49
spshreya <shreya.pandey.emip@gmail.com>
2020-02-19 06:08:14 -0800

    day7

---
+++ matlib.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+adj1=input("Enter an adjective:")
+name=input("Enter a name:")
+noun=input("Enter a noun:")
+adj2=input("Enter another adjective:")
+plNoun=input("Enter a plural noun (living being):")
+
+print("And there's this "+adj1+" guy named "+name+" who brings a "+noun+ " to all the " +plNoun+ " who have been "+adj2)
+
